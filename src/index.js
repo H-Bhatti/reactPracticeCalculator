84e27904b3175a4d024713ff6a0ffca14a21e453
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-07-14 17:31:33 +0400

    props learning

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import Card from "./cards";
 
 const data = [
   {
-    imageSrc: "Rick & Morty",
-    sName: "https://www.netflix.com/title/80014749",
-    sLink: "https://wallpapercave.com/uwp/uwp4412040.jpeg",
+    sName: "Rick & Morty",
+    sLink: "https://www.netflix.com/title/80014749",
+    imageSrc: "https://wallpapercave.com/uwp/uwp4412040.jpeg",
   },
   {
-    imageSrc: "Stranger Things",
-    sName: "https://www.netflix.com/title/80057281",
-    sLink: "https://wallpapercave.com/uwp/uwp3621169.png",
+    sName: "Stranger Things",
+    sLink: "https://www.netflix.com/title/80057281",
+    imageSrc: "https://wallpapercave.com/uwp/uwp3621169.png",
   },
   {
-    imageSrc: "breaking Bad",
-    sName: "https://www.netflix.com/title/70143836",
-    sLink: "https://wallpapercave.com/fuwp/uwp4353669.jpeg",
+    sName: "breaking Bad",
+    sLink: "https://www.netflix.com/title/70143836",
+    imageSrc: "https://wallpapercave.com/fuwp/uwp4353669.jpeg",
   },
 ];
 
@@ -25,19 +25,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Card
-      imageSrc="https://wallpapercave.com/uwp/uwp4412040.jpeg"
-      sName="Rick & Morty"
-      sLink="https://www.netflix.com/title/80014749"
+      sName={data[0].sName}
+      sLink={data[0].sLink}
+      imageSrc={data[0].imageSrc}
     />
     <Card
-      sName="breaking Bad"
-      sLink="https://www.netflix.com/title/70143836"
-      imageSrc="https://wallpapercave.com/fuwp/uwp4353669.jpeg"
+      sName={data[1].sName}
+      sLink={data[1].sLink}
+      imageSrc={data[1].imageSrc}
     />
     <Card
-      sName="Stranger Things"
-      sLink="https://www.netflix.com/title/80057281"
-      imageSrc="https://wallpapercave.com/uwp/uwp3621169.png"
+      sName={data[2].sName}
+      sLink={data[2].sLink}
+      imageSrc={data[2].imageSrc}
     />
   </>
 );
